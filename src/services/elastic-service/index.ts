fcb92f0c4cb5abb693eda56b8d3895bc26a85128
serafettin <mail@serafettin.net>
2022-12-08 17:19:52 +0300

    Update index.ts

--- src/services/elastic-service/index.ts
+++ src/services/elastic-service/index.ts
@@ -191,7 +191,7 @@ class ElasticService {
 			if (l.highlight["links.content"]) {
 				l._source.content = l.highlight["links.content"]
 					.join("... ")
-					.replace(/(\r\n|\n|\r|\\n)/gm, "");
+					.replace(/(\r\n|\n|\r|\\n|<br>|<br \/>|<a>|<\/a>)/gm, "");
 			}
 
 			if (l.highlight["links.title"]) {
